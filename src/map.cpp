c67f253b1f26f1208eff5748b8227bf0c4c0a23b
lchsk <mjlechowski@gmail.com>
2016-09-23 21:28:48 +0100

    Draw map from json file

--- src/map.cpp
+++ src/map.cpp
@@ -16,23 +16,45 @@ namespace ks
         _tile_w = 32;
         _tile_h = 32;
 
-        _tiles_cols = 70;
-        _tiles_rows = 50;
-
         json map = data_loader->load_map("map1.json");
 
-        for (int r = 0; r < _tiles_rows; r++) {
-            for (int c = 0; c < _tiles_cols; c++) {
-                auto s = _resource_mgr->get_spritesheet("tiles")
-                    .get(0, 4);
+        _tiles_cols = map["metadata"]["columns"];
+        _tiles_rows = map["metadata"]["rows"];
 
-                _tiles.push_back(
+        std::vector<ks::Spritesheet*> spritesheets;
+
+        json j_sheets = map["spritesheets"];
+        json j_data = map["data"];
+        json j_land = j_data["land"];
+
+        for (json::iterator it = j_sheets.begin(); it != j_sheets.end(); ++it) {
+            spritesheets.push_back(&_resource_mgr->get_spritesheet(it.value()));
+        }
+
+        int r = 0;
+        int c = 0;
+
+        for (json::iterator it = j_land.begin(); it != j_land.end(); ++it) {
+            auto item = it.value();
+
+            int spritesheet_id = item[0];
+            int tile_id = item[1];
+
+            auto s = spritesheets[spritesheet_id]->get(tile_id);
+
+            _tiles.push_back(
                     std::make_unique<ks::Tile>(
                         *s,
-                        r * _tile_w,
-                        c * _tile_h
+                        c * _tile_w,
+                        r * _tile_h
                         )
                     );
+
+            c++;
+
+            if (c == _tiles_cols) {
+                c = 0;
+                r++;
             }
         }
     }
