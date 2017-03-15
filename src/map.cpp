993f887a3655842d96a1ef24d14fcbf72d1a6ba4
lchsk <mjlechowski@gmail.com>
2017-03-15 22:59:26 +0000

    Move path finding out of Map. Add example of path finding for a single unit

--- src/map.cpp
+++ src/map.cpp
@@ -3,15 +3,16 @@
 namespace ks
 {
     Map::Map(
-        std::shared_ptr<ks::ResourceMgr> resource_mgr,
-        std::shared_ptr<ks::DataLoader> data_loader,
-        std::shared_ptr<ks::GameWindow> window,
+        std::shared_ptr<ks::ResourceMgr>& resource_mgr,
+        std::shared_ptr<ks::DataLoader>& data_loader,
+        std::shared_ptr<ks::GameWindow>& window,
+        std::shared_ptr<std::vector<std::shared_ptr<ks::Unit> > >& units,
         std::string map_name)
         : _map_name(map_name),
           _resource_mgr(resource_mgr),
           _data_loader(data_loader),
           _window(window),
-          _gps(std::make_unique<ks::Gps>()),
+          _units(units),
           _graph(std::make_unique<ks::Graph>())
     {
         load();
@@ -28,8 +29,6 @@ namespace ks
 
         _tiles_cols = j_map["metadata"]["columns"];
         _tiles_rows = j_map["metadata"]["rows"];
-
-        _v_col = _tiles_cols * _v_per_tile;
 
         _window->set_map_size(ks::TILE * _tiles_cols, ks::TILE * _tiles_rows);
 
@@ -78,109 +77,30 @@ namespace ks
             }
         }
 
-        for (int R = 0; R < _tiles_rows * 4 * 8; R += 8) {
-            for (int C = 0; C < _tiles_cols * 4 * 8; C += 8) {
-                boost::add_vertex(
-                    std::make_shared<ks::Vertex>(
-                        C,
-                        R,
-                        0, 0), *_graph);
-            }
-        }
+        _graph->init(_tiles_rows * ks::TILE, _tiles_cols * ks::TILE);
 
-        _build_initial_graph();
+        // Find path:
 
-		vertex_t s = boost::vertex(0, *_graph);
-		vertex_t goal = boost::vertex(37, *_graph);
+        std::shared_ptr<std::vector<ks::Vertex> > path
+            = std::make_shared<std::vector<ks::Vertex> >();
 
-        auto ss = (*_graph)[0];
-        auto gg = (*_graph)[37];
+        _graph->find_path(path, 0, 60);
 
-        std::vector<vertex_t> p(boost::num_vertices(*_graph));
-        std::vector<int> d(boost::num_vertices(*_graph));
-
-        dijkstra_shortest_paths(*_graph, s,
-                                predecessor_map(boost::make_iterator_property_map(p.begin(), get(boost::vertex_index, *_graph))).
-                                distance_map(boost::make_iterator_property_map(d.begin(), get(boost::vertex_index, *_graph))));
-
-        std::vector<ks::vertex_t> path;
-        ks::vertex_t current = goal;
-
-        while (current != s) {
-            path.push_back(current);
-
-            current = p[current];
-        }
-
-        path.push_back(s);
-
-        std::vector<boost::graph_traits<ks::Graph>::vertex_descriptor >::iterator it;
-
-        for (it = path.begin(); it != path.end(); it++) {
-            auto t = (*_graph)[*it];
-
-            auto l = std::make_unique<sf::RectangleShape>();
-
-            l->setFillColor(sf::Color::Red);
-            l->setSize(sf::Vector2f(8, 8));
-            l->setPosition(sf::Vector2f(t->x, t->y));
-
-            _path.push_back(std::move(l));
-        }
-        std::cout << std::endl;
+        (*_units)[0]->set_path(path);
     }
 
     void Map::update(sf::Time delta)
     {
     }
 
-    void Map::render(
-        sf::RenderWindow& window,
-        std::vector<std::shared_ptr<ks::Unit> >& units)
+    void Map::render(sf::RenderWindow& window)
     {
         for (auto& tile : _tiles) {
             tile->render(window);
         }
 
-        for (auto& unit : units) {
+        for (auto& unit : *_units) {
             unit->render(window);
-        }
-
-        for (auto& l : _path) {
-            window.draw(*l);
-        }
-    }
-
-    void Map::_add_edge(const int v1, const int v2) const
-    {
-        if (v2 < 0) return;
-
-        auto edge = boost::edge(v1, v2, *_graph);
-
-        if (edge.second) return;
-
-        boost::add_edge(v1, v2, 1, *_graph);
-    }
-
-    bool Map::_is_connected(const int v1, const int v2) const
-    {
-        return boost::edge(v1, v2, *_graph).second;
-    }
-
-    void Map::_build_initial_graph() const
-    {
-        _gps->v_cnt = boost::num_vertices(*_graph);
-        _gps->v_col = _v_col;
-
-        for (int v = 0; v < _gps->v_cnt; v++) {
-            _add_edge(v, _gps->get_nw(v));
-            _add_edge(v, _gps->get_n(v));
-            _add_edge(v, _gps->get_ne(v));
-            _add_edge(v, _gps->get_e(v));
-            _add_edge(v, _gps->get_se(v));
-            _add_edge(v, _gps->get_s(v));
-            _add_edge(v, _gps->get_sw(v));
-            _add_edge(v, _gps->get_w(v));
         }
     }
 }
--- src/map.h
+++ src/map.h
@@ -3,8 +3,6 @@
 
 #include <SFML/Graphics.hpp>
 
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-
 #include "resource_mgr.h"
 #include "animation.h"
 #include "tile.h"
@@ -17,29 +15,29 @@ namespace ks
 {
     /* Side of each tile */
     const int TILE = 32;
-    class Gps;
 
     class Map
     {
     public:
-        Map(std::shared_ptr<ks::ResourceMgr> resource_mgr,
-            std::shared_ptr<ks::DataLoader> data_loader,
-            std::shared_ptr<ks::GameWindow> window,
+        Map(std::shared_ptr<ks::ResourceMgr>& resource_mgr,
+            std::shared_ptr<ks::DataLoader>& data_loader,
+            std::shared_ptr<ks::GameWindow>& window,
+            std::shared_ptr<std::vector<std::shared_ptr<ks::Unit> > >& units,
             std::string map_name);
         ~Map();
 
         void update(sf::Time delta);
-        void render(sf::RenderWindow& window,
-                    std::vector<std::shared_ptr<ks::Unit> >& units);
+        void render(sf::RenderWindow& window);
 
     private:
-        std::unique_ptr<ks::Graph> _graph;
-
         void load();
+
+        std::unique_ptr<ks::Graph> _graph;
 
         std::shared_ptr<ks::ResourceMgr> _resource_mgr;
         std::shared_ptr<ks::GameWindow> _window;
         std::shared_ptr<ks::DataLoader> _data_loader;
+        std::shared_ptr<std::vector<std::shared_ptr<ks::Unit> > > _units;
 
         std::vector<
             std::unique_ptr<ks::Tile>
@@ -58,99 +56,6 @@ namespace ks
         const int _v_per_tile = 4;
 
         std::string _map_name;
-
-        void _add_edge(const int v1, const int v2) const;
-        void _build_initial_graph() const;
-
-        bool _is_connected(const int v1, const int v2) const;
-
-        std::unique_ptr<ks::Gps> _gps;
-
-        // Debugging
-
-        std::vector<std::unique_ptr<sf::RectangleShape> > _path;
-    };
-
-    class Gps {
-    public:
-        int v_col;
-        int v_cnt;
-
-        int get_nw(const int v) const
-        {
-            if (is_top_row(v) || is_left_col(v)) return -1;
-
-            return v - v_col - 1;
-        }
-
-        int get_n(const int v) const
-        {
-            if (is_top_row(v)) return -1;
-
-            return v - v_col;
-        }
-
-        int get_ne(const int v) const
-        {
-            if (is_top_row(v) || is_right_col(v)) return -1;
-
-            return v - v_col + 1;
-        }
-
-        int get_e(const int v) const
-        {
-            if (is_right_col(v)) return -1;
-
-            return v + 1;
-        }
-
-        int get_se(const int v) const
-        {
-            if (is_bottom_row(v) || is_right_col(v)) return -1;
-
-            return v + v_col + 1;
-        }
-
-        int get_s(const int v) const
-        {
-            if (is_bottom_row(v)) return -1;
-
-            return v + v_col;
-        }
-
-        int get_sw(const int v) const
-        {
-            if (is_bottom_row(v) || is_left_col(v)) return -1;
-
-            return v + v_col - 1;
-        }
-
-        int get_w(const int v) const
-        {
-            if (is_left_col(v)) return -1;
-
-            return v - 1;
-        }
-
-        bool is_top_row(const int v) const
-        {
-            return v < v_col;
-        }
-
-        bool is_bottom_row(const int v) const
-        {
-            return v >= v_cnt - v_col && v < v_cnt;
-        }
-
-        bool is_left_col(const int v) const
-        {
-            return v % v_col == 0;
-        }
-
-        bool is_right_col(const int v) const
-        {
-            return v % v_col == (v_col - 1);
-        }
     };
 }
 
